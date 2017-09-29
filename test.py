8217834b6d293f9be26247f9327fdc0a4e73eadf
Joel Johnson <johnson.joel.86@gmail.com>
2017-09-29 01:07:07 -0600

    make things a bit more interesting

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from retrogine.lib import *
 
+i = 0
 
 @update
 def update(delta):
@@ -8,9 +9,18 @@ def update(delta):
 
 @draw
 def draw():
+    global i
+    i = i + 1
+    i = i % 140
     cls()
-    spr(0, 30, 30)
-    spr(0, 50, 30)
+    spr(0, 100, 100, 0)
+    spr(0, 120, 100, 0)
+    spr(0, 140, 100, 0)
+    spr(0, 160, 100, 0)
+    spr(0, 180, 100, 0)
+    spr(0, 200, 100, 0)
+    spr(0, 80 + i, 100, 1)
+
 
 
 retrogine(1000, 750, 16, 16, show_fps=True, enable_os_mouse=True, desired_fps=60)
