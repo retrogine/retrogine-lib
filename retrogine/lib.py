8e31a7975b8a567e647b7f1cd6a13396890b149d
Joel Johnson <johnson.joel.86@gmail.com>
2017-10-01 00:02:39 -0600

    manage the rate at which @update is called.
    Added text and fps helper functions

--- retrogine/lib.py
+++ retrogine/lib.py
@@ -1,7 +1,7 @@
-from typing import Callable
+from typing import Callable, Tuple
 import retrogine.data_loader as data_loader
 from pygame.rect import Rect
-import pygame.time
+from pyglet.clock import Clock
 import pygame
 import sys
 
@@ -9,22 +9,36 @@ _default_color = (0, 0, 0)
 _update: Callable = None
 _draw: Callable = None
 _screen = None
+_debug_font: pygame.font.Font = None
+_last_debug_pos: Tuple = (0, 0)
+_clock: Clock
+_update_rate: int
 
 (_sprites, _palettes) = data_loader.load_data_file('test.data')
 
 
 def retrogine(
         window_width: int,
-        window_height: int
+        window_height: int,
+        update_rate: int = 30
 ):
     global _screen
+    global _debug_font
+    global _clock
+    global _last_debug_pos
+    global _update_rate
+    _update_rate = update_rate
+
     pygame.init()
+
+    _debug_font = pygame.font.SysFont("monospace", 15)
 
     size = window_width, window_height
 
     _screen = pygame.display.set_mode(size)
 
-    clock = pygame.time.Clock()
+    _clock = Clock()
+    _clock.schedule_interval(_update, 1 / update_rate)
 
     while True:
         for event in pygame.event.get():
@@ -32,15 +46,32 @@ def retrogine(
                 pygame.quit()
                 sys.exit()
 
-        _update(clock.tick())
+        _last_debug_pos = (0, -_debug_font.get_height())
         _draw()
         pygame.display.flip()
+        _clock.tick()
+        # print(_clock.get_fps())
+
+
+def fps() -> int:
+    global _clock
+    return int(_clock.get_fps())
 
 
 def spr(sprite_number: int, x: int, y: int, palette_number=0):
     sprite = _sprites.get(sprite_number)
     surface = sprite.get_image(_palettes[palette_number])
     _screen.blit(surface, Rect(x, y, 16, 16))
+
+
+def text(txt: str, color: Tuple = (255, 255, 255), position: Tuple = None):
+    global _last_debug_pos
+    if not position:
+        position = (_last_debug_pos[0], _last_debug_pos[1] + _debug_font.get_height() + 1)
+    _last_debug_pos = position
+
+    label = _debug_font.render(str(txt), 1, color)
+    _screen.blit(label, position)
 
 
 def cls(color=_default_color):
--- test.py
+++ test.py
@@ -1,18 +1,21 @@
 from retrogine.lib import *
 
 i = 0
+d = 0
 
 
 @update
 def update(delta):
-    pass
+    global i
+    global d
+    d = delta
+    i = i + 1
+    i = i % 140
 
 
 @draw
 def draw():
     global i
-    i = i + 1
-    i = i % 140
     cls()
     spr(0, 0, 0, 0)
     spr(0, 100, 100, 0)
@@ -22,6 +25,8 @@ def draw():
     spr(0, 180, 100, 0)
     spr(0, 200, 100, 0)
     spr(0, 80 + i, 100, 1)
+    text(fps())
+    text(d)
 
 
 retrogine(1000, 750)
