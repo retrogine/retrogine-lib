edb1c860c042b41ac8570e484f8dafe1b52b2004
Joel Johnson <johnson.joel.86@gmail.com>
2017-10-01 13:46:53 -0600

    making the test a little more interesting

--- test.py
+++ test.py
@@ -3,13 +3,17 @@ from retrogine.lib import *
 p_x = 0
 p_y = 0
 d = 0
+palette = 0
+was_down = False
 
 
 @update
 def update(delta):
+    global was_down
     global p_x
     global p_y
     global d
+    global palette
     d = delta
 
     if btn(Buttons.UP):
@@ -22,26 +26,31 @@ def update(delta):
     if btn(Buttons.RIGHT):
         p_x = p_x + 1
 
+    if btn(Buttons.A):
+        if not was_down:
+            was_down = True
+            palette = palette + 1
+    else:
+        if was_down:
+            was_down = False
+
 
 @draw
 def draw():
     global p_x
     cls()
 
-    for y in range(12):
-        for x in range(20):
-            spr(1, x * 16, y * 16, (x + y) % 2)
+    # for y in range(12):
+    #     for x in range(20):
+    #         spr(1, x * 16, y * 16, (x + y) % 2)
 
-    spr(1, 16, 0, 1)
-    spr(1, 32, 0, 0)
-    spr(1, 48, 0, 1)
-    spr(0, 100, 100, 0)
-    spr(0, 120, 100, 0)
-    spr(0, 140, 100, 0)
-    spr(0, 160, 100, 0)
-    spr(0, 180, 100, 0)
-    spr(0, 200, 100, 0)
-    spr(0, p_x, p_y, 1)
+    spr(0, 100, 100, (palette + 0) % 2)
+    spr(0, 120, 100, (palette + 1) % 2)
+    spr(0, 140, 100, (palette + 0) % 2)
+    spr(0, 160, 100, (palette + 1) % 2)
+    spr(0, 180, 100, (palette + 0) % 2)
+    spr(0, 200, 100, (palette + 1) % 2)
+    spr(1, p_x, p_y, (palette + 0) % 2)
     debug(str(fps()))
     debug(str(d))
     debug(str(btns()))
