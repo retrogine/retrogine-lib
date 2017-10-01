5c2348af3546042f41902e7fb63726e12af64011
Joel Johnson <johnson.joel.86@gmail.com>
2017-10-01 00:41:38 -0600

    draw a grid border to test aspect ratio

--- test.py
+++ test.py
@@ -17,7 +17,18 @@ def update(delta):
 def draw():
     global i
     cls()
-    spr(0, 0, 0, 0)
+
+    for j in range(16):
+        spr(1, j * 16, 0, j % 2)
+        spr(1, j * 16, 14*16, j % 2)
+
+    for j in range(15):
+        spr(1, 0, j * 16, j % 2)
+        spr(1, 15 * 16, j * 16, (j+1) % 2)
+
+    spr(1, 16, 0, 1)
+    spr(1, 32, 0, 0)
+    spr(1, 48, 0, 1)
     spr(0, 100, 100, 0)
     spr(0, 120, 100, 0)
     spr(0, 140, 100, 0)
